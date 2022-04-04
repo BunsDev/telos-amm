fb26a54a3d7541d58e4f7b36869fc1976da95038
Buns <bunsthedev@gmail.com>
2022-04-03 20:21:29 -0500

    update: Socials

--- interface/src/components/Menu/index.tsx
+++ interface/src/components/Menu/index.tsx
@@ -93,15 +93,15 @@ export default function Menu() {
       </StyledMenuButton>
       {open && (
         <MenuFlyout>
-          <MenuItem id="link" href="https://moonbeam.network/">
+          <MenuItem id="link" href="https://soulswap.finance/">
             <Home size={14} />
             {t('Website')}
           </MenuItem>
-          <MenuItem id="link" href="https://discord.gg/PfpUATX">
+          <MenuItem id="link" href="https://discord.com/invite/DQjChB6Wa6">
             <MessageCircle size={14} />
             {t('discord')}
           </MenuItem>
-          <MenuItem id="link" href="https://github.com/PureStake/moonbeam">
+          <MenuItem id="link" href="https://github.com/BunsDev/telos-amm">
             <Code size={14} />
             {t('code')}
           </MenuItem>
